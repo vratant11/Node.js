468ec3dc6c723e88affbce5d9368cb7dea7e66e7
vratant11 <vratant2012061@akgec.ac.in>
2022-11-24 23:39:48 +0530

    Node

--- npmMod/selfevents/index.js
+++ npmMod/selfevents/index.js
@@ -3,5 +3,5 @@ const EventEmittr = require("stream");
 const event = new EventEmittr();
 event.emit("sayMyName");
 event.on("sayMyName", () => {
-  console.log("My name is Vratant Singh");
+  console.log("My name is vratant Singh");
 });
