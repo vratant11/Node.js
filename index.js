b68a25874e46e4a13784e129af400b68079fe3f6
vratant11 <vratant2012061@akgec.ac.in>
2022-11-26 21:41:26 +0530

    Express started

--- index.js
+++
@@ -1,5 +0,0 @@
-// const fs = require("fs");
-// fs.writeFileSync("read.txt" , "Welcome to my channel");
-// fs.appendFileSync("read.txt" , "how are you");
-const os = require("os");
-console.log(os.arch());
