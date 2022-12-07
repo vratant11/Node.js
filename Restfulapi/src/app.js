c6965d189ff64aced7251059602a326f1c690f79
vratant11 <vratant2012061@akgec.ac.in>
2022-12-08 00:11:10 +0530

    Rest Api

--- Restfulapi/src/app.js
+++ Restfulapi/src/app.js
@@ -5,20 +5,29 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 //create a new students
-app.post("/students", (req, res) => {
-  console.log(req.body);
-  const user = new Student(req.body);
-  user
-    .save()
-    .then(() => {
-      res.send(user);
-     return res.status(201);
-    })
-    .catch((e) => {
-      console.log(e);
-     return res.status(400);
-    });
+// app.post("/students", (req, res) => {
+//   console.log(req.body);
+//   const user = new Student(req.body);
+//   user
+//     .save()
+//     .then(() => {
+//       res.send(user);
+//      return res.status(201);
+//     })
+//     .catch((e) => {
+//       console.log(e);
+//      return res.status(400);
+//     });
 //   res.send("Hello");
+// });
+app.post("/students", async (req, res) => {
+  try {
+    const user = new Student(req.body);
+    const createUser = await user.save();
+    res.status(201).send(createUser);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 app.listen(port, () => {
